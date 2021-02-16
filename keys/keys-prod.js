4ad37d4aeda1671e7f6df95a5f3bfcb09808729c
Timyr <timyrforwork@gmail.com>
2021-02-16 16:22:23 +0300

    'Shop'

--- keys/keys-prod.js
+++ keys/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_DB_URI: proccess.env.MONGO_DB_URI,
+    MONGO_DB_URI: process.env.MONGO_DB_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
